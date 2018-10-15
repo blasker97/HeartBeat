9ff9d5a8a393bd2d4f71515cf67b22aa0a793d94
Brian <bjlp7c@mail.missouri.edu>
2018-10-15 10:25:17 -0500

    Initial Commit

---
+++ HeartBeat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HeartBeat
+//
+//  Created by Lasker, Brian (MU-Student) on 10/15/18.
+//  Copyright © 2018 Lasker, Brian (MU-Student). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
